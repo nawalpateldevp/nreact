c0c9a8c992582372c77e549cad7068c60967fdd8
Nawal Patel Dev <nawalpatel.devp@gmail.com>
2025-10-06 00:31:34 +0530

    How to create html element in react without jsx

--- App.js
+++ App.js
@@ -1,28 +1,39 @@
-        /***
-         * 
-         * <div id="parent">
-         *  <div id="child">
-         *      <h1>This is h1 tag</h1>
-         *      <h2>This is h2 tag sibling</h2>
-         *  </div>
-         * </div>
-         * 
-         * 
-         * ReactElement(Object) => HTML(Browser understands)
-         * 
-         */
-        
-        const parent = React.createElement('div', {id:'parent'}, 
-                        React.createElement('div', {id:'child'}, 
-                            [React.createElement('h1', {key:"heading"}, 'This is h1 tag'),
-                            React.createElement('h2', {key:"subheading"}, 'This is h2 tag sibling')]
-                        )
-                    );
-        
-        const heading = React.createElement('h1', 
-            {id:"heading", attriname:"attributevalue"}, 
-            'Hello, React!');
+/***
+ * 
+ * <div id="parent">
+ *  <div id="child">
+ *      <h1>This is h1 tag</h1>
+ *      <h2>This is h2 tag sibling</h2>
+ *  </div>
+ *  <div id="child2">
+ *      <h1>This is h1 tag</h1>
+ *      <h2>This is h2 tag sibling</h2>
+ *  </div>
+ * </div>
+ * 
+ * 
+ * ReactElement(Object) => HTML(Browser understands)
+ * 
+ */
 
-        const root = ReactDOM.createRoot(document.getElementById('root'));
-        console.log(parent);
-        root.render(parent);
+const parent = React.createElement('div',
+    { id: 'parent' },
+    [
+        React.createElement('div', { id: 'child' },
+            [React.createElement('h1', { key: "heading" }, 'This is h1 tag from child 1'),
+            React.createElement('h2', { key: "subheading" }, 'This is h2 tag sibling')]
+        ),
+        React.createElement('div', { id: 'child2' },
+            [React.createElement('h1', { key: "heading" }, 'This is h1 tag'),
+            React.createElement('h2', { key: "subheading" }, 'This is h2 tag sibling')]
+        )
+    ]
+);
+
+const heading = React.createElement('h1',
+    { id: "heading", attriname: "attributevalue" },
+    'Hello, React!');
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+console.log(parent);
+root.render(parent);
